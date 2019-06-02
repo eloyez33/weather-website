74bdd810b92a4f73a47ded47b99f049f60e2e64c
eloy-sanchez <eloy.sanchez@lloydsbanking.com>
2019-06-02 18:37:57 +0100

    Change de forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, logitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, 
-                body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.'
+                body.daily.data[0].summary+' La temperatura actual es '+body.currently.temperature+' grados. Hay un '+body.currently.precipProbability+'% de probabilidad de lluvia.'
             )
         }
     })
